8fdb47c19aeec0b88f09f9cb991fafff60d5c6b3
Vini6703 <vinicius.oliveira6703@gmail.com>
2024-06-15 12:22:20 -0300

    Conteudo da aula do dia 15 de junho de 2024

--- Eclipse/Lição1/M24-Java1-B/src/br/com/senaisp/bauru/vinicius/licao06/Carta.java
+++ Eclipse/Lição1/M24-Java1-B/src/br/com/senaisp/bauru/vinicius/licao06/Carta.java
@@ -2,7 +2,7 @@ package br.com.senaisp.bauru.vinicius.licao06;
 
 public class Carta {
 	public static final String[] NAIPES = {"♦","♠","♥","♣"};
-	public static final String[] NUMEROS = {"A","2","3","4","5","6","7","8","9","10","J","Q","K"}
+	public static final String[] NUMEROS = {"A","2","3","4","5","6","7","8","9","10","J","Q","K"};
 	private String naipe;
 	private String numero;
 	//Constructor
@@ -20,11 +20,12 @@ public class Carta {
 	public String toString() {
 		String txt = "┌───────┐\n"+//1
 					 "│##     │\n"+//2
-					 "│       Ꜳ       │ \n"+//3
+					 "│   $   │ \n"+//3
 					 "│     ##│\n"+//4
 					 "└───────┘\n";//5
-		txt = txt.replaceFirst("##", numero);
-		
+		txt = txt.replaceFirst("##", numero + (numero.equals("10") ? "" : ""));
+		txt = txt.replace("$$", naipe);
+		txt = txt.replaceFirst("##", (numero.equals("10") ? "" : ""));
 		return txt;
 	}
 	
